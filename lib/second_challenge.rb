77dbc0615abb34675b55642c02cd630a7d8187ba
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-25 15:06:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -41,16 +41,16 @@ def second_challenge
       # :status => "alive"
     },
     :heroine_friends=> [
-      {
-        # :name => "Steven",
-        # :age => "30",
-        # :attitude => "confused"
-      },
-      {
-        # :name => "Nurse",
-        # :age => "44",
-        # :attitude => "worried"
-      }
+      # {
+      #   # :name => "Steven",
+      #   # :age => "30",
+      #   # :attitude => "confused"
+      # },
+      # {
+      #   # :name => "Nurse",
+      #   # :age => "44",
+      #   # :attitude => "worried"
+      # }
     ]
   }
 }
