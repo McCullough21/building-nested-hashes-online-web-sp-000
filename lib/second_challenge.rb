f4909364b3b34d66ffe69f38158f640f22f519de
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-25 15:01:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,22 +4,22 @@ def second_challenge
       :patriarch => {
         :name => "Lord Montague",
         :age => "53"
-      }
+      },
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
-      }
+      },
       :hero => {
         :name => "Romeo",
         :age => "15"
         :status => "Alive"
-      }
+      },
       :hero_friends => [
         {
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
-        }
+        },
         {
           :name => "Mercutio",
           :age => "18",
